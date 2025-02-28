8d17d067d7d79e0ae998684b6999a3eb6b05d6d5
dmitry950 <dhtendgost@gmail.com>
2025-02-28 23:19:48 +0300

    commit

---
+++ calculator ded dovlet.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int a, b, sum = 0;
+    cin >> a >> b;
+    if (a >= 1 && a <= b && b <= 1000000) {
+        int x[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+        for (int i = a; i <= b; i++) {
+            int num = i;
+            while (num > 0) {
+                sum += x[num % 10];
+                num /= 10;
+            }
+        }
+        cout << sum;
+    }
+    else {
+        cout << "нарушены условия: 1 ≤ a ≤ b ≤ 10^6" << endl;
+    }
+    return 0;
+}
