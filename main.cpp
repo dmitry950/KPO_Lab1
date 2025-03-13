4ae2060db8fab693fb024f20ecc6001e6b64c304
dmitry950 <dhtendgost@gmail.com>
2025-03-14 01:07:06 +0300

    Разрешен конфликт

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 int main() {
     cout << "Hello World!\n";
-<<<<<<< HEAD
     cout << "Add Changes in feature/conflict";
-=======
     cout << "Add Changes";
     cout << "Add Changes again";
->>>>>>> ed40a4109ee40716ae92080e5eb629543741034a
         return 0;
 }
